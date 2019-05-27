3d7dfc6167a99f66e2ec6568b6ebab75ddb2ce6a
Mir HB Rahman <ahadcr0@gmail.com>
2019-05-27 21:10:02 +0600

    Added all streams components and navigation routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+import StreamCreate from './streams/StreamCreate'
+import StreamEdit from './streams/StreamEdit'
+import StreamDelete from './streams/StreamDelete'
+import StreamList from './streams/StreamList'
+import StreamShow from './streams/StreamShow'
+import Header from './Header'
 
 class App extends Component {
   render() {
     return (
-      <div>
-        App
+      <div className="ui container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route path='/' exact component={StreamList} />
+            <Route path='/streams/new' exact component={StreamCreate} />
+            <Route path='/streams/edit' exact component={StreamEdit} />
+            <Route path='/streams/delete' exact component={StreamDelete} />
+            <Route path='/streams/show' exact component={StreamShow} />
+          </div>
+        </BrowserRouter>
       </div>
     )
   }
---
+++ client/src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+  return (
+    <div className="ui secondary pointing menu">
+      <Link to="/" className="item">Streamy</Link>
+      <div className="right menu">
+        <Link to="/" className="item">All Streams</Link>
+      </div>
+    </div>
+  )
+}
