9548b712539d615681a05afb38111731e8971555
Mir HB Rahman <ahadcr0@gmail.com>
2019-05-30 15:39:55 +0600

    Added action and reducers for streams CRUD

--- client/src/components/streams/StreamCreate.js
+++ client/src/components/streams/StreamCreate.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Field, reduxForm } from 'redux-form'
+import { connect } from 'react-redux'
+import { createStream } from '../../actions'
 
 class StreamCreate extends Component {
 
@@ -24,8 +26,8 @@ class StreamCreate extends Component {
     );
   }
 
-  onSubmit(formValues) {
-    console.log(formValues);
+  onSubmit = (formValues) => {
+    this.props.createStream(formValues);
   }
 
   render() {
@@ -53,7 +55,9 @@ const validate = (formValues) => {
   return errors;
 }
 
-export default reduxForm({
+const formWrapped = reduxForm({
   form: 'streamCreate',
   validate
 })(StreamCreate);
+
+export default connect(null, { createStream })(formWrapped);
--- client/src/components/streams/StreamList.js
+++ client/src/components/streams/StreamList.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
+import { fetchStreams } from '../../actions';
 
-export default function StreamList() {
-  return (
-    <div>
-      StreamList
-    </div>
-  )
+class StreamList extends Component {
+  componentDidMount() {
+    this.props.fetchStreams();
+  }
+  render() {
+    return (
+      <div>
+        StreamList
+      </div>
+    )
+  }
 }
+
+
+export default connect(null, { fetchStreams })(StreamList);
