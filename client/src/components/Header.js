10408a58fbda2bb0bb8ca057f2e895451cb47465
Mir HB Rahman <ahadcr0@gmail.com>
2019-06-03 22:28:54 +0600

    Added RTMP server to live stream video

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ import GoogleAuth from './GoogleAuth'
 
 export default function Header() {
   return (
-    <div className="ui secondary pointing menu">
+    <div className="ui secondary pointing menu" style={{ marginTop: '10px' }}>
       <Link to="/" className="item">Streamy</Link>
       <div className="right menu">
         <Link to="/" className="item">All Streams</Link>
